2bc51036bc100aacf1476e240a5da22feb355e1a
Martin R <sizlaksystems@gmail.com>
2016-03-11 15:28:36 +0100

    Added Float64ToString2DecimalPlaces function

--- lib.go
+++ lib.go
@@ -11,8 +11,14 @@ func StringToFloat64(myString string) float64 {
 	retFloat, err := strconv.ParseFloat(strings.Trim(myString, "\n"), 64)
 
 	if err != nil {
-		fmt.Println("ERROR: StringToFloat( ", myString, " ): ", err)
+		fmt.Println("ERROR: StringToFloat64( ", myString, " ): ", err)
 	}
 
 	return retFloat
+}
+
+func Float64ToString2DecimalPlaces(myFloat float64) string {
+
+	// Format the float64 to 2 decimal places
+	return strconv.FormatFloat(myFloat, 'f', 2, 64)
 }
--- lib_test.go
+++ lib_test.go
@@ -10,3 +10,10 @@ func TestStringToFloat64(t *testing.T) {
 	t.Log("StringToFloat64(\"33.65\") = ", myFloat)
 	t.Errorf("Error: Testing StringToFloat64")
 }
+
+func TestFloat64ToString2DecimalPlaces(t *testing.T) {
+
+	myFloat := Float64ToString2DecimalPlaces(33.65676)
+	t.Log("Float64ToString2DecimalPlaces(33.65676) = ", myFloat)
+	t.Errorf("Error: Testing Float64ToString2DecimalPlaces")
+}
